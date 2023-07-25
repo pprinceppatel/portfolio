6acbb8103d058a0bac74f415ff6bcc7dee0f53e0
pprinceppatel <pprinceppatel8383@gmail.com>
2023-07-25 19:54:23 +0530

    commit portfolio

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -12,7 +12,7 @@ function Github() {
         username="pprinceppatel"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="aqua"
         fontSize={16}
       />
     </Row>
